66ea048df3aaf49c8d123713ab76552f6b98e3bb
LilChara999 <annamarienkova33@gmail.com>
2020-02-29 15:06:23 +0100

    Файл авторизации пользователя с использованием токенов.

--- models/user.js
+++ models/user.js
@@ -1,27 +1,43 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
-const userSchema = new Schema({
+const { Schema } = mongoose;
+
+const UsersSchema = new Schema({
   email: String,
-  username: String,
-  password: String
-},
-{
-  timestamps: {
-    createdAt: 'createdAt',
-    updatedAt: 'updatedAt'
-  }
-})
+  hash: String,
+  salt: String,
+});
 
-const User = mongoose.model('user', userSchema)
-module.exports = User
+UsersSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
-module.exports.hashPassword = async (password) => {
-  try {
-    const salt = await bcrypt.genSalt(10)
-    return await bcrypt.hash(password, salt)
-  } catch(error) {
-    throw new Error('Hashing failed', error)
-  }
+UsersSchema.methods.validatePassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+UsersSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
 }
+
+UsersSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT(),
+  };
+};
+
+mongoose.model('Users', UsersSchema);
